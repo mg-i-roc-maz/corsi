0b0e6d703db7f71f6ae7d034cd980d09e1002a53
Rocco Mazzeo <116784643+mg-i-roc-maz@users.noreply.github.com>
2025-10-15 18:28:39 +0200

    Aggiungi programma per la conversione di immagini BMP a scala di grigi

---
+++ programmazione_c/esempi/lezione_9/bandw.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma pack(push, 1)
+typedef struct
+{
+    unsigned short type;
+    unsigned int size;
+    unsigned short reserved1, reserved2;
+    unsigned int offset;
+} BMPHeader;
+
+typedef struct
+{
+    unsigned int size;
+    int width, height;
+    unsigned short planes;
+    unsigned short bitCount;
+    unsigned int compression;
+    unsigned int sizeImage;
+    int xPelsPerMeter, yPelsPerMeter;
+    unsigned int clrUsed, clrImportant;
+} BMPInfoHeader;
+#pragma pack(pop)
+
+unsigned char to_bw(unsigned char r, unsigned char g, unsigned char b)
+{
+    // Luminance formula
+    return (unsigned char)(0.299 * r + 0.587 * g + 0.114 * b);
+}
+
+int main(void)
+{
+    const char *input_file = "input.bmp";
+    const char *output_file = "output.bmp";
+
+    FILE *fin = fopen(input_file, "rb");
+    if (!fin)
+    {
+        perror("Errore apertura file input");
+        return 1;
+    }
+
+    FILE *fout = fopen(output_file, "wb");
+    if (!fout)
+    {
+        perror("Errore apertura file output");
+        fclose(fin);
+        return 1;
+    }
+
+    BMPHeader header;
+    BMPInfoHeader info;
+    fread(&header, sizeof(BMPHeader), 1, fin);
+    fread(&info, sizeof(BMPInfoHeader), 1, fin);
+
+    if (header.type != 0x4D42 || info.bitCount != 24)
+    {
+        printf("Solo immagini BMP 24 bit supportate.\n");
+        fclose(fin);
+        fclose(fout);
+        return 1;
+    }
+
+    fwrite(&header, sizeof(BMPHeader), 1, fout);
+    fwrite(&info, sizeof(BMPInfoHeader), 1, fout);
+
+    int width = info.width;
+    int height = info.height;
+    int row_padded = (width * 3 + 3) & (~3);
+    unsigned char *row = malloc(row_padded);
+
+    fseek(fin, header.offset, SEEK_SET);
+    fseek(fout, header.offset, SEEK_SET);
+
+    for (int i = 0; i < height; i++)
+    {
+        fread(row, 1, row_padded, fin);
+        for (int j = 0; j < width; j++)
+        {
+            unsigned char b = row[j * 3];
+            unsigned char g = row[j * 3 + 1];
+            unsigned char r = row[j * 3 + 2];
+            unsigned char bw = to_bw(r, g, b);
+            row[j * 3] = row[j * 3 + 1] = row[j * 3 + 2] = bw;
+        }
+        fwrite(row, 1, row_padded, fout);
+    }
+
+    free(row);
+    fclose(fin);
+    fclose(fout);
+    printf("Conversione completata.\n");
+    return 0;
+}
